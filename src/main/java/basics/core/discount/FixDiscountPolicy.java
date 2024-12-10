4639fd8137ae6b8e74aba6e63b69c00838b21418
Youshin Kim <ro117youshin@gmail.com>
2024-12-10 15:08:44 +0900

    feat: 주문과 할인 도메인 추가

---
+++ src/main/java/basics/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,19 @@
+package basics.core.discount;
+
+import basics.core.member.Grade;
+import basics.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmount = 1000; // 1000원 할인
+
+    @Override
+    public int discount(Member member, int price) {
+
+        if(member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
