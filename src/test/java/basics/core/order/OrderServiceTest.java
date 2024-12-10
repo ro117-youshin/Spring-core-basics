3925005d940a94903ae9c860a6a4879d039a7ce6
Youshin Kim <ro117youshin@gmail.com>
2024-12-10 15:22:37 +0900

    feat: 주문과 할인 도메인 테스트

---
+++ src/test/java/basics/core/order/OrderServiceTest.java
@@ -0,0 +1,24 @@
+package basics.core.order;
+
+import basics.core.member.Grade;
+import basics.core.member.Member;
+import basics.core.member.MemberService;
+import basics.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void creatOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
